a04a153c2b8137151f7d8f668bfe3ae6344e0a47
scalewithpankaj <scalewithpankaj@gmail.com>
2026-06-07 00:17:54 -0400

    Update index.ts
    port fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+// import dotenv from "dotenv";
+// // Load up env file which contains credentials
+// dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+// import { Server } from "./server";
+
+// const server = new Server();
+// server.listen(8080);
 import dotenv from "dotenv";
 // Load up env file which contains credentials
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
@@ -5,4 +13,9 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 import { Server } from "./server";
 
 const server = new Server();
-server.listen(8080);
+
+// Check for Render's dynamic system port, otherwise fall back to 8080
+const port = process.env.PORT || 8080;
+
+server.listen(Number(port));
+console.log(`Voice server successfully listening on port ${port}`);
